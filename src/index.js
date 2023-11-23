f31761b397d442816af4bffe142765e4fe3c7f1b
Jesus David <20200750@uthh.edu.mx>
2023-11-23 02:12:24 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+export * from './service-worker';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorkerRegistration.register()
+serviceWorker.register();
