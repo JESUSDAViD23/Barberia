80441dfec36d679fe93a898334f1c8a179f4c3da
Jesus David <20200750@uthh.edu.mx>
2023-11-23 03:59:31 -0600

    Update header.js

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import { Anchor, Drawer, Button } from 'antd';
 
 const { Link } = Anchor;
@@ -14,32 +15,25 @@ function AppHeader() {
     setVisible(false);
   };
 
-  const handleClick = () => {
-    // Cierra el Drawer cuando se hace clic en un enlace en dispositivos móviles
-    if (window.innerWidth < 768) {
-      onClose();
-    }
-  };
-
   return (
     <div className="container-fluid">
-      <div className="header d-flex justify-content-between align-items-center">
+      <div className="header">
         <div className="logo">
           <i className="fas fa-bolt"></i>
           <a href="http://google.com">Tech</a>
         </div>
-        <div className="d-none d-md-block"> {/* Oculto en dispositivos pequeños */}
-          <Anchor targetOffset={65}>
+        <div className="mobileHidden">
+          <Anchor targetOffset="65">
             <Link href="#hero" title="Inicio" />
-            <Link href="#about" title="Acerca de" />
-            <Link href="#feature" title="Características" />
-            <Link href="#works" title="Cómo trabajamos" />
+            <Link href="#about" title="Acercade" />
+            <Link href="#feature" title="Caracteristicas" />
+            <Link href="#works" title="Como trabajamos" />
             <Link href="#faq" title="Preguntas frecuentes" />
-            <Link href="#pricing" title="Precios" />
+            <Link href="#pricing" title="Precio" />
             <Link href="#contact" title="Contacto" />
           </Anchor>
         </div>
-        <div className="d-block d-md-none"> {/* Visible solo en dispositivos pequeños */}
+        <div className="mobileVisible">
           <Button type="primary" onClick={showDrawer}>
             <i className="fas fa-bars"></i>
           </Button>
@@ -49,14 +43,14 @@ function AppHeader() {
             onClose={onClose}
             visible={visible}
           >
-            <Anchor targetOffset={15}>
-              <Link href="#hero" title="Inicio" onClick={handleClick} />
-              <Link href="#about" title="Acerca de" onClick={handleClick} />
-              <Link href="#feature" title="Características" onClick={handleClick} />
-              <Link href="#works" title="Cómo trabajamos" onClick={handleClick} />
-              <Link href="#faq" title="Preguntas frecuentes" onClick={handleClick} />
-              <Link href="#pricing" title="Precios" onClick={handleClick} />
-              <Link href="#contact" title="Contacto" onClick={handleClick} />
+            <Anchor targetOffset="25">
+              <Link href="#hero" title="Inicio" />
+              <Link href="#about" title="Acerca de" />
+              <Link href="#feature" title="Caracteristicas" />
+              <Link href="#works" title="Como trabajamos" />
+              <Link href="#faq" title="Preguntas frecuentes" />
+              <Link href="#pricing" title="Precios" />
+              <Link href="#contact" title="Contact" />
             </Anchor>
           </Drawer>
         </div>
